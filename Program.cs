00874f38457e26d1835b24a9b46b89df50adedbe
niko057 <nicatnamazov57.nn@gmail.com>
2024-04-02 11:49:15 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+namespace NameSpace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] array1 = { 1, 2, 3, 4, 5 };
+            //int[] array2 = { 3,2,4};
+
+            //bool result = IsArrayContains(array1, array2);
+
+            //Console.WriteLine(result);
+
+            Student student = new Student();
+
+            student.Run();
+
+         
+
+          
+           
+        }
+
+        static bool IsArrayContains(int[] array1, int[] array2)
+        {
+            if (array2.Length > array1.Length)
+                return false;
+
+            for (int i = 0; i <= array1.Length - array2.Length; i++)
+            {
+                bool found = true;
+                for (int j = 0; j < array2.Length; j++)
+                {
+                    if (array1[i + j] != array2[j])
+                    {
+                        found = false;
+                        break;
+                    }
+                }
+                if (found)
+                    return true;
+            }
+            return false;
+        }
+
+
+    }
+
+}
+
+
---
+++ Student.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameSpace
+{
+    internal class Student :User
+    {
+        public int Grade { get; set; }
+
+        public void Run()
+        {
+            string[] user = { "Nicat", "Pasha", "Ali", "Vuqar" };
+
+            string[] pass = { "123", "456", "987" };
+
+            Console.WriteLine("Zehmet olmasa istifadeci adinizi daxil edin...");
+            string username = Console.ReadLine();
+            Console.WriteLine("Zehmet olmasa parol daxil edin...");
+            string password = Console.ReadLine();
+            bool check = false;
+
+            for (int i = 0; i < user.Length; i++)
+            {
+                for (int j = 0; j < pass.Length; j++)
+                {
+                    if (username == user[i])
+                    {
+                        check = true;
+
+                    }
+                    else if (pass[j] == password)
+                    {
+                        check = true;
+                    }
+                    else
+                    {
+                        check = false;
+                    }
+
+                }
+                if (check==true)
+                {
+                    Console.WriteLine(check);
+                    Console.WriteLine("Grade");
+                }
+                else
+                {
+                    Console.WriteLine("Bele bir istifadeci tapilmadi");
+                }
+               
+                break;
+            }
+        }
+    }
+}
